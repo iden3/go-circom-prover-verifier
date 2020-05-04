603919442917d4f240552ca44a60493df0546859
Eduard S <eduard@iden3.io>
2020-05-04 14:09:18 +0200

    Update scripts to use node_modules locally

--- testdata/compile-circuits.sh
+++ testdata/compile-circuits.sh
@@ -3,27 +3,27 @@
 compile_and_ts_and_witness() {
   echo $(date +"%T") "circom circuit.circom --r1cs --wasm --sym"
   itime="$(date -u +%s)"
-  circom circuit.circom --r1cs --wasm --sym
+  ../node_modules/.bin/circom circuit.circom --r1cs --wasm --sym
   ftime="$(date -u +%s)"
   echo "	($(($(date -u +%s)-$itime))s)"
 
   echo $(date +"%T") "snarkjs info -r circuit.r1cs"
-  snarkjs info -r circuit.r1cs
+  ../node_modules/.bin/snarkjs info -r circuit.r1cs
 
   echo $(date +"%T") "snarkjs setup"
   itime="$(date -u +%s)"
-  snarkjs setup
+  ../node_modules/.bin/snarkjs setup
   echo "	($(($(date -u +%s)-$itime))s)"
   echo $(date +"%T") "trusted setup generated"
 
   sed -i 's/null/["0","0","0"]/g' proving_key.json
 
   echo "calculating witness"
-  snarkjs calculatewitness --wasm circuit.wasm --input inputs.json --witness witness.json
+  ../node_modules/.bin/snarkjs calculatewitness --wasm circuit.wasm --input inputs.json --witness witness.json
 
   echo $(date +"%T") "snarkjs generateverifier"
   itime="$(date -u +%s)"
-  snarkjs generateverifier
+  ../node_modules/.bin/snarkjs generateverifier
   echo "	($(($(date -u +%s)-$itime))s)"
   echo $(date +"%T") "generateverifier generated"
 }
